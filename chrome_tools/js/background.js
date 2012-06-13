373693d2f5cf1994ba34d1f033a66ebb44463429
Cheng Liang <233_clcl@163.com>
2012-06-14 06:30:44 +0800

    background

--- chrome_tools/js/background.js
+++ chrome_tools/js/background.js
@@ -174,9 +174,40 @@ chrome.extension.onRequest.addListener(function (request, sender, sendResponse) 
 		sendResponse({result:request.temp_data.length});
 		return;
 	}
+	if (reqtype == 'append_temp_data') {
+		var temp_data = JSON.parse(window.localStorage.getItem('temp_data'));
+		if(temp_data.constructor == Array)
+			temp_data = temp_data.concat(request.temp_data);
+		else
+			temp_data = request.temp_data;
+		window.localStorage.setItem('temp_data', JSON.stringify(temp_data));
+		sendResponse({result:temp_data.length});
+		return;
+	}
+	if (reqtype == 'pop_temp_data') {
+		var temp_data = JSON.parse(window.localStorage.getItem('temp_data'));
+		var id = '';
+		if(temp_data.constructor == Array && temp_data.length > 0)
+			id = temp_data.pop();
+			
+		window.localStorage.setItem('temp_data', JSON.stringify(temp_data));
+		sendResponse({result:id});
+		return;
+	}
 	if (reqtype == 'get_temp_data') {
 		var temp_data = JSON.parse(window.localStorage.getItem('temp_data'));
 		sendResponse({result:temp_data});
+		return;
+	}
+	if (reqtype == 'get_temp_flag') {
+		var temp_flag = JSON.parse(window.localStorage.getItem('temp_flag'));
+		sendResponse({result:temp_flag});
+		return;
+	}
+
+	if (reqtype == 'set_temp_flag') {
+		window.localStorage.setItem('temp_flag', JSON.stringify(request.temp_flag));
+		sendResponse({result:request.temp_flag});
 		return;
 	}
 	
@@ -250,3 +281,194 @@ var obj = plugin.CreateObject("test");
 
 var ret = plugin.push2Client("test","5501");
 console.log(ret);*/
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Number of milliseconds in a second.
+ * @type {number}
+ */
+MILLISECONDS_IN_A_SECOND = 1000;
+
+/**
+ * Duration of desktop notification in milliseconds.
+ * @type {number}
+ */
+NOTIFICATION_DURATION = 10000;
+
+/**
+ * Starts, resets and pauses timer, updates timer values at regular intervals,
+ * shows notifications at specified time.
+ * @constructor
+ */
+var Timer = function() {
+  // Store interval ID for setInterval.
+  this.countdown = 0;
+
+  // Stores seconds value.
+  this.seconds = 0;
+
+  // Indicate if timer is running.
+  this.running = false;
+
+  // Function to update time values in popup.
+  this.popupNotifier = null;
+
+  // Stores desktop notification reference.
+  this.notification = null;
+
+  // Stores notification timeout ID.
+  this.notificationTimeout = 0;
+};
+
+/**
+ * Restores last timer session if present
+ * (in case when browser was closed while timer running).
+ */
+Timer.prototype.restoreLastSession = function() {
+  // Stores time elapsed since timer started (in seconds).
+  var timeGap;
+  if (window.localStorage.getItem('seconds') == undefined) {
+    return;
+  }
+  // Get last session start time in milliseconds.
+  var startTime = window.localStorage.getItem('start-time');
+
+  // Get duration of session in seconds.
+  var seconds = window.localStorage.getItem('seconds');
+
+  // Store current system time in milliseconds.
+  var currentTime = new Date().getTime();
+  // Get time difference in seconds.
+  timeGap = Math.floor((currentTime - startTime) / MILLISECONDS_IN_A_SECOND);
+  if (timeGap > seconds) {
+    this.reset();
+  } else {
+    // Browsing time left.
+    this.seconds = seconds - timeGap;
+    this.running = true;
+    this.start();
+  }
+};
+
+/**
+ * Cancels any existing desktop notification.
+ */
+Timer.prototype.cancelNotification = function() {
+  if (this.notificationTimeout) {
+    clearTimeout(this.notificationTimeout);
+    this.notification.cancel();
+  }
+};
+
+/**
+ * Sets timer values and stores the session duration and current system time.
+ * @param {String} seconds Value of seconds set in popup.
+ */
+Timer.prototype.setTime = function(seconds) {
+  this.seconds = Number(seconds);
+  localStorage.setItem('seconds', this.seconds);
+  // Store current system time.
+  localStorage.setItem('start-time', new Date().getTime());
+  this.cancelNotification();
+};
+
+/**
+ * Updates timer, shows animation every minute and notify popup (if active)
+ * to update its display.
+ */
+Timer.prototype.tick = function() {
+  this.seconds--;
+  this.updateBadge();
+};
+
+/**
+ * Starts timer.
+ */
+Timer.prototype.start = function() {
+  this.running = true;
+  this.updateBadge();
+  var self = this;
+  // Set interval of one second.
+  this.countdown = setInterval(function() {
+    self.tick();
+  }, MILLISECONDS_IN_A_SECOND);
+};
+
+/**
+ * Resets timer.
+ */
+Timer.prototype.reset = function() {
+  if (this.countdown) {
+    clearInterval(this.countdown);
+  }
+  if (this.seconds) {
+    // If timer being reset using reset button.
+    chrome.browserAction.setBadgeText({text: ''});
+    chrome.browserAction.setTitle({title: chrome.i18n.getMessage('title')});
+    this.cancelNotification();
+  }
+  localStorage.removeItem('start-timer');
+  localStorage.removeItem('seconds');
+  this.running = false;
+};
+
+/**
+ * Pauses timer.
+ */
+Timer.prototype.pause = function() {
+  if (this.countdown) {
+    clearInterval(this.countdown);
+  }
+};
+
+/**
+ * Shows desktop notification.
+ * @param {string} htmlFilePath HTML notification file path.
+ */
+Timer.prototype.notify = function(htmlFilePath) {
+  // Cancel any existing notification.
+  /*
+  this.cancelNotification();
+  this.notification = webkitNotifications.createHTMLNotification(htmlFilePath);
+  this.notification.show();*/
+  var title = 'test';
+  var msg = 'time up';
+  this.notification = webkitNotifications.createHTMLNotification(
+      'notification.html?title='+escape(title)+'&msg='+escape(msg)
+    );	
+    this.notification.show();
+};
+
+/**
+ * Updates badge text/color and show notification at specified time.
+ */
+Timer.prototype.updateBadge = function() {
+  var sed = this.seconds;
+
+  // Store local reference to current instance.
+  var self = this;
+  if (sed == 0) {
+    // If session time up.
+    this.notify('../views/alert_timeup.html');
+    // Cancel notification and reset timer after 10 sec.
+    this.notificationTimeout = setTimeout(function() {
+      self.notification.cancel();
+    }, NOTIFICATION_DURATION);
+    this.reset();
+  }
+};
+
+var timer = new Timer();
+timer.setTime(10);
+timer.restoreLastSession();
+
